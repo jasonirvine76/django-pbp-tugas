593de042b975f72e69971c220a0a9aa2ccc1254f
jasonirvine76 <jason.irvine@ui.ac.id>
2022-09-09 21:24:31 +0700

    mengerjakan poin 1 sampai 3

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
 # TODO: Implement Routings Here
+from django.contrib import admin
+from django.urls import path, include
+from katalog.views import katalog
+
+urlpatterns = [
+    path('', katalog, name='katalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from katalog.models import CatalogItem
+
 # TODO: Create your views here.
+
+def katalog(request):
+    catalog_item_list = CatalogItem.objects.all()
+    context = {
+        'nama': 'Jason Irvine Mahendra Putra',
+        'npm' : '2106750654',
+        'data': catalog_item_list,
+    }
+    return render(request, 'katalog.html', context)
